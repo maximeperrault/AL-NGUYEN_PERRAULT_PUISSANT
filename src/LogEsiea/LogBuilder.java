27b14fcf055f4fb1b60009409ab1d1d64e2fa297
BlackyFox <puissant@et.esiea.fr>
2015-02-18 15:54:58 +0100

    Concatenation des methodes debug, info, error et wtf

---
+++ src/LogEsiea/LogBuilder.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package LogEsiea;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * @author Antoine
+ *
+ */
+class LogBuilder {
+
+	public void LogginBuilder(DateFormat df, String loggingFrom, String msg, char destination, String lvl, int line){
+		Date d = new Date();
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(df.format(d));
+		sb.append(" [CLASS=");
+		sb.append(loggingFrom+":");
+		sb.append(line);
+		sb.append(" LEVEL=");
+		sb.append(Levels.valueOf(lvl));
+		sb.append(" MESSAGE= ");
+		sb.append(msg);
+		sb.append("]");
+		WriteLog w = new WriteLog();
+		w.write(destination, sb.toString());
+	}
+	
+}
--- src/LogEsiea/LogEsiea.java
+++ src/LogEsiea/LogEsiea.java
@@ -8,9 +8,10 @@ public class LogEsiea {
 
 	private final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 	private final String loggingFrom;
-	private final WriteLog write = new WriteLog();
 	private char destination = 't';
 	private int lvl = 0;
+	private LogBuilder lb = new LogBuilder();
+	
 	
 	public LogEsiea() {
 		StackTraceElement [] s = new RuntimeException().getStackTrace();
@@ -19,77 +20,29 @@ public class LogEsiea {
 	
 	public void error(String msg){
 		if(this.lvl <= Levels.valueOf("e").ordinal()){
-			Date d = new Date();
 			StackTraceElement [] s = new RuntimeException().getStackTrace();
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append(df.format(d));
-			sb.append(" [CLASS=");
-			sb.append(loggingFrom+":");
-			sb.append(s[1].getLineNumber());
-			sb.append(" LEVEL=");
-			sb.append(Levels.valueOf("e"));
-			sb.append(" MESSAGE= ");
-			sb.append(msg);
-			sb.append("]");
-			write.write(this.destination, sb.toString());
+			lb.LogginBuilder(this.df, this.loggingFrom, msg, this.destination, "e", s[1].getLineNumber());
 		}
 	}
 	
 	public void debug(String msg){
 		if(this.lvl <= Levels.valueOf("d").ordinal()){
-			Date d = new Date();
 			StackTraceElement [] s = new RuntimeException().getStackTrace();
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append(df.format(d));
-			sb.append(" [CLASS=");
-			sb.append(loggingFrom+":");
-			sb.append(s[1].getLineNumber());
-			sb.append(" LEVEL=");
-			sb.append(Levels.valueOf("d"));
-			sb.append(" MESSAGE= ");
-			sb.append(msg);
-			sb.append("]");
-			write.write(this.destination, sb.toString());
+			lb.LogginBuilder(this.df, this.loggingFrom, msg, this.destination, "d", s[1].getLineNumber());
 		}
 	}
 	
 	public void info(String msg){
 		if(this.lvl <= Levels.valueOf("i").ordinal()){
-			Date d = new Date();
 			StackTraceElement [] s = new RuntimeException().getStackTrace();
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append(df.format(d));
-			sb.append(" [CLASS=");
-			sb.append(loggingFrom+":");
-			sb.append(s[1].getLineNumber());
-			sb.append(" LEVEL=");
-			sb.append(Levels.valueOf("i"));
-			sb.append(" MESSAGE= ");
-			sb.append(msg);
-			sb.append("]");
-			write.write(this.destination, sb.toString());
+			lb.LogginBuilder(this.df, this.loggingFrom, msg, this.destination, "i", s[1].getLineNumber());
 		}
 	}
 	
 	public void wtf(String msg){
 		if(this.lvl <= Levels.valueOf("w").ordinal()){
-			Date d = new Date();
 			StackTraceElement [] s = new RuntimeException().getStackTrace();
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append(df.format(d));
-			sb.append(" [CLASS=");
-			sb.append(loggingFrom+":");
-			sb.append(s[1].getLineNumber());
-			sb.append(" LEVEL=");
-			sb.append(Levels.valueOf("w"));
-			sb.append(" MESSAGE= ");
-			sb.append(msg);
-			sb.append("]");
-			write.write(this.destination, sb.toString());
+			lb.LogginBuilder(this.df, this.loggingFrom, msg, this.destination, "w", s[1].getLineNumber());
 		}
 	}
 
